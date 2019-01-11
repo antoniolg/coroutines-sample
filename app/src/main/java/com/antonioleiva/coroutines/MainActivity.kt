d0394a36d686c8102be57109c3b305d18472ef23
Antonio Leiva <antonioleivag@gmail.com>
2019-01-11 12:02:33 +0100

    Dispatcher for the main coroutine

--- app/src/main/java/com/antonioleiva/coroutines/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/coroutines/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun doLogin(username: String, password: String) {
 
-        coroutine {
+        coroutine(Dispatchers.Main) {
             progress.visibility = View.VISIBLE
 
             val user = suspended { userService.doLogin(username, password) }
--- app/src/main/java/com/antonioleiva/coroutines/extensions.kt
+++ app/src/main/java/com/antonioleiva/coroutines/extensions.kt
@@ -1,9 +1,11 @@
 package com.antonioleiva.coroutines
 
 import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 // NOTE: These extensions are written for academic purposes. Not meant to be used on a real project.
 
-fun coroutine(block: suspend CoroutineScope.() -> Unit) = GlobalScope.launch(context = Dispatchers.Main, block = block)
+fun coroutine(context: CoroutineContext, block: suspend CoroutineScope.() -> Unit) =
+    GlobalScope.launch(context, block = block)
 
 suspend fun <T> suspended(block: suspend CoroutineScope.() -> T) = withContext(Dispatchers.Default, block)
